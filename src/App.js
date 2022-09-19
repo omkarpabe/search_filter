bbae5088936eb5ca9cde80b07f5c24e7de82eece
omkarpabe <omkarpabe@gmail.com>
2022-09-19 19:07:11 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from "react";
+import { Users } from  "./users";
+
 
 function App() {
+
+  const [query, setQuery] = useState("");
+
+  console.log(query);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <input 
+       type="text"  
+       placeholder="Search..." 
+       className="search" 
+       onChange={(e) => setQuery(e.target.value)}
+       />
+
+       <ul className="list">
+        {Users.map((user) => (
+        <li key={user.id} className="listItems">
+          {user.first_name}
+          </li>
+        
+        ))}
+
+
+       </ul>
+
+
     </div>
   );
 }
